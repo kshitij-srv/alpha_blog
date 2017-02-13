5d77032932ed158a5580cf2e015e3d7f3c8a567e
kshitij <kshitijsrv@gmail.com>
2017-02-13 07:42:07 -0500

    alpha_blog rails application first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  
+  def home    #defining action for home page
+    
+  
+  end
+  
+  def about    #defining action for about page
+    
+    
+  end
+end
