e1bd3ba24632d397e329e3eeba1aded3256466dd
kshitij <kshitijsrv@gmail.com>
2017-03-09 01:21:39 -0500

    complete authentication system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,10 @@
 class PagesController < ApplicationController
   
   def home    #defining action for home page
-    
-  
+    redirect_to user_path(current_user.id) if logged_in?
   end
   
   def about    #defining action for about page
-    
-    
+     
   end
 end
