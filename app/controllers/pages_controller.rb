4a4053ba3d49a8d7dcf9054b99b31e065c53e514
kshitij <kshitijsrv@gmail.com>
2017-03-09 01:46:21 -0500

    user logged in after sign up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   
   def home    #defining action for home page
-    redirect_to user_path(current_user.id) if logged_in?
+    redirect_to user_path(current_user) if logged_in?
   end
   
   def about    #defining action for about page
